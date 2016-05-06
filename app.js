86e2603fd9d683fcd8144beb31d77d08821592ea
HgbrdCeline <Hagbard5555@gmail.com>
2016-05-05 22:35:27 -0700

    Added favicon.ico and route

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
  res.sendFile(__dirname + '/index.html');
+ console.log('Sent index.html')
 });
+
+app.get('/favicon.ico', function(req, res){
+ res.sendFile(__dirname + '/favicon.ico');
+ console.log('Sent favicon.ico')
+});
+
 io.on('connection', function(socket){
  socket.on('chat message', function(msg){
- io.emit('chat message', msg);
+   io.emit('chat message', msg);
  });
 });
+
 http.listen(port, function(){
  console.log('listening on ' + port);
 });
